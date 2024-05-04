73bc2080aa844a190f9d18d668cc13d27e4c1209
Pedro Filho <psfilho81@gmail.com>
2024-05-04 11:43:38 -0300

    Create DeckOfCards02.java

---
+++ Exemplos/section1_4/DeckOfCards02.java
@@ -0,0 +1,44 @@
+package section1_4;
+
+public class DeckOfCards02 {
+   public static void main(String[] args) {
+      // declare, create and initialize arrays
+      String[] SUITS = {
+            "Clubs", "Diamonds", "Hearts", "Spades"
+      };
+
+      String[] RANKS = {
+            "2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "Jack", "Queen", "King", "Ace"
+      };
+
+      // print random card
+      // int i = (int) (Math.random() * RANKS.length);
+      // int j = (int) (Math.random() * SUITS.length);
+      // System.out.println(RANKS[i] + " of " + SUITS[j]);
+
+      // Setting array values at compile time
+      String[] deck = new String[RANKS.length * SUITS.length];
+      for (int i = 0; i < RANKS.length; i++) {
+         for (int j = 0; j < SUITS.length; j++) {
+            deck[SUITS.length * i + j] = RANKS[i] + " of " + SUITS[j];
+            System.out.println(RANKS[i] + " of " + SUITS[j]);
+         }
+      }
+      System.out.println();
+
+      // embaralhar cartas
+      int n = deck.length;
+      for (int i = 0; i < n; i++) {
+         int r = i + (int) (Math.random() * (n - i));
+         String temp = deck[r];
+         deck[r] = deck[i];
+         deck[i] = temp;
+      }
+
+      // print array value
+      for (int i = 0; i < n; i++) {
+         System.out.println(deck[i]);
+      } // end for print
+   } // end main
+} // end class
