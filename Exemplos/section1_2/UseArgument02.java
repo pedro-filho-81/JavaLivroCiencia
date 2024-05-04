4a138e085bc7cb71f81d6f3816ebb4a4e0d7dc77
Pedro Filho <psfilho81@gmail.com>
2024-05-04 10:41:39 -0300

    Create UseArgument02.java

---
+++ Exemplos/section1_2/UseArgument02.java
@@ -0,0 +1,13 @@
+public class UseArgument02 {
+   public static void main(String[] args) {
+
+      /*
+       * System.out.print("Olá, ");
+       * System.out.print(args[0]);
+       * System.out.println(". How are you?");
+       */
+
+      // modo simplificado
+      System.out.println("Hi, " + args[0] + ". How are yoy?");
+   }
+}
