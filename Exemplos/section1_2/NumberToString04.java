41241457cc9d2a79a4e6da2af247f8f1285dc853
Pedro Filho <psfilho81@gmail.com>
2024-05-04 10:41:14 -0300

    Create NumberToString04.java

---
+++ Exemplos/section1_2/NumberToString04.java
@@ -0,0 +1,16 @@
+/**
+ * NumberToString04
+ */
+public class NumberToString04 {
+
+   public static void main(String[] args) {
+      // variables
+      String a = "1234"; // var a recebe uma Sering
+      String b = "99"; // var b recebe uma Strine
+      String c = a + b; // o sinal de + "CONCATENA" Strings (string + string = outra string)
+
+      // display
+      System.out.println("Soma de Strings:");
+      System.out.println(a + " + " + b + " = " + c);
+   }
+}
