ad62de8ab8ff9821b0756da7628c57631ff9b688
Pedro Filho <psfilho81@gmail.com>
2024-05-04 10:42:25 -0300

    Create Programa1.java

---
+++ Exemplos/section1_4/Programa1.java
@@ -0,0 +1,19 @@
+package section1_4;
+
+/*
+ * Este programa recebe dois argumentos de linha de 
+ * comando m e n e produz uma amostra de m dos 
+ * inteiros de 0 a n-1. Este processo é útil não 
+ * apenas em loterias estaduais e locais, mas em 
+ * aplicações científicas de todos os tipos. Se o 
+ * primeiro argumento for igual ao segundo, o 
+ * resultado será uma permutação aleatória dos 
+ * inteiros de 0 a n-1. Se o primeiro argumento for 
+ * maior que o segundo, o programa terminará com uma 
+ * ArrayOutOfBoundsException.
+ */
+public class Programa1 {
+   public static void main(String[] args) {
+
+   }
+}
