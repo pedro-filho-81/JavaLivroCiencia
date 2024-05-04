0af3e766f1325753f74b1dc91201b3451ef04bbe
Pedro Filho <psfilho81@gmail.com>
2024-05-04 10:42:03 -0300

    Create tenHello02.java

---
+++ Exemplos/section1_3/tenHello02.java
@@ -0,0 +1,17 @@
+package section1_3;
+
+public class tenHello02 {
+   public static void main(String[] args) {
+      System.out.println("1st Hello");
+      System.out.println("2nd Hello");
+      System.out.println("3rd Hello");
+      // variable
+      int n = 4;
+      // loop while
+      while (n <= 10) {
+         System.out.println(n + "th Hello");
+         // pós-incremento
+         n++;
+      }
+   }
+}
