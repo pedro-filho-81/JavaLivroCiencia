65663a5674bf6fa0519adf1aead6820122d662b7
Pedro Filho <psfilho81@gmail.com>
2024-05-07 18:14:06 -0300

    Update ArrayExample01.java

--- Exemplos/section1_4/ArrayExample01.java
+++ Exemplos/section1_4/ArrayExample01.java
@@ -1,19 +1,19 @@
 package section1_4;
 
-import java.util.Scanner;
-
+/*
+ * Copyright © 2000–2017, Robert Sedgewick and 
+ * Kevin Wayne.
+ * Last updated: Fri Oct 20 14:12:12 EDT 2017.
+ */
 public class ArrayExample01 {
    public static void main(String[] args) {
-      // create input object
-      Scanner input = new Scanner(System.in);
 
-      // data entre
-      System.out.print("Digite o tamanho do array: ");
       // variable receive value of the user
-      int n = input.nextInt();
+      int n = Integer.parseInt(args[0]);
 
       // declare and create array
       double[] a = new double[n];
+
       // for loop to initialize array
       for (int i = 0; i < n; i++) {
          // array receive random value
@@ -36,11 +36,16 @@ public class ArrayExample01 {
       double min = 1;
       // for loop to find the maximum and the minimus array value
       for (int i = 0; i < n; i++) {
+         // if max less then array
          if (max < a[i])
+            // max receive array value
             max = a[i];
+         // if min greater then array
          if (min > a[i])
+            // min receive array value
             min = a[i];
       }
+      // print result
       System.out.println("max = " + max);
       System.out.println("min = " + min);
 
@@ -59,6 +64,7 @@ public class ArrayExample01 {
       double[] b = new double[n];
       // for loop to copy to other array
       for (int i = 0; i < n; i++) {
+         // array b receive array value a
          b[i] = a[i];
       } // end for copy
 
